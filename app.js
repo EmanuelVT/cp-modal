d710d30cd306b05d150401c4e7b47ad006efc2e3
Emanuel Vasconcelos <vascovj@hotmail.com>
2021-12-06 07:44:43 -0600

    Fixed the arrow icon to go down and up and changed the dropdwon click event to only affect the down arrow

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 let modalButton = document.querySelector(".modal-button");
-let path = document.getElementsByClassName("path-header-container");
+let path = document.getElementsByClassName("dropdown-arrow");
 
 //Loop to make dropdown menus work
 for(var i = 0; i < path.length; i++){
     path[i].onclick = function(){
         let content = this.nextElementSibling;
+        
+        
         if(content.style.display == 'block'){
-            content.style.display = 'none'
+            content.style.display = 'none';
+            this.innerHTML = "<i class='lni lni-chevron-down'></i>";
+            
         } else{
             content.style.display = 'block';
+            this.innerHTML = "<i class='lni lni-chevron-up'></i>";
         }
     }
 }
